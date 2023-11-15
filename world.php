5e6c2eb3dfd6c98d1d6bbaa53020e3406e87397b
Keneel <tkeneel@gmail.com>
2023-11-15 13:10:20 -0500

    Update

--- world.php
+++ world.php
@@ -12,14 +12,11 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 
 if (isset($_GET['country'])) {
   $country = $_GET['country'];
-
-  // Prepare and execute the query
   $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
   $stmt->execute(['country' => "%$country%"]);
-
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } else {
-  // Default action if 'country' parameter is not set
+
   $results = [];
 }
 
