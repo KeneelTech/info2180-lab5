6ee88724d9323a5614dfba18e760ee557d1a968a
Keneel <tkeneel@gmail.com>
2023-11-15 10:10:18 -0500

    Completed Part 4

--- world.php
+++ world.php
@@ -25,8 +25,19 @@ if (isset($_GET['country'])) {
 
 ?>
 
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence</th>
+    <th>Head of State</th>
+  </tr>
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?= htmlspecialchars($row['name']) ?></td>
+      <td><?= htmlspecialchars($row['continent']) ?></td>
+      <td><?= htmlspecialchars($row['independence_year']) ?></td>
+      <td><?= htmlspecialchars($row['head_of_state']) ?></td>
+    </tr>
+  <?php endforeach; ?>
+</table>
