0551a089ec47ef90fe9202904a75ab3955c1dcf0
Keneel <tkeneel@gmail.com>
2023-11-15 09:46:48 -0500

    Completed Part 3

--- world.js
+++ world.js
@@ -1,1 +1,18 @@
 "use strict";
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const result = document.getElementById('result');
+    document.getElementById('lookup').addEventListener('click', function() {
+        // Fetch the data when the button is clicked
+        const country = document.querySelector('input[name="country"]').value;
+        const url = `http://localhost/info2180-lab5/world.php?country=${encodeURIComponent(country)}`;
+
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                result.innerHTML = data;
+            })
+            .catch(error => console.error('Error:', error));
+    });
+});
--- world.php
+++ world.php
@@ -1,15 +1,30 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+
 $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if (isset($_GET['country'])) {
+  $country = $_GET['country'];
+
+  // Prepare and execute the query
+  $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+  $stmt->execute(['country' => "%$country%"]);
+
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} else {
+  // Default action if 'country' parameter is not set
+  $results = [];
+}
 
 ?>
+
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
